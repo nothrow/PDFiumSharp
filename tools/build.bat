1107b41966cb4722560ebec35ce7abd95b9cfc3f
Vita Tauer <t@uer.cz>
2021-03-24 12:17:53 +0100

    Fixed build, downgraded pdfium binaries

--- tools/build.bat
+++ tools/build.bat
@@ -1,7 +1,12 @@
 pushd %~dp0\..
 
-dotnet build -c Release dirs.proj
-dotnet pack -c Release --no-build src\nothrow.PDFium
-dotnet pack -c Release --no-build src\nothrow.PDFium.Binaries
+
+dotnet restore
+
+dotnet build -c Release src\nothrow.PDFium.Binaries
+dotnet build -c Release src\nothrow.PDFium
+
+dotnet pack -c Release src\nothrow.PDFium
+dotnet pack -c Release src\nothrow.PDFium.Binaries
 
 popd
